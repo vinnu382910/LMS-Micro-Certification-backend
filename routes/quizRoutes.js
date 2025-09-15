9a644f706d793bf6ab1f57a1d8c0e2b695585776
vinnu382910 <vinaykalva712@gmail.com>
2025-09-16 04:57:53 +0530

    updated quizRoute.js

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getQuizQuestions, submitQuiz } = require("../controllers/quizController");
 const { verifyToken } = require("../middleware/authMiddleware");
 
-router.get("/:quizId", verifyToken, getQuizQuestions);
+router.get("/:quizId", getQuizQuestions);
 router.post("/submit", verifyToken, submitQuiz);
 
 module.exports = router;
